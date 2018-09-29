c9cbcb7c0af6ea52f5e31dea8e02b659aa9c5a3e
Jinghua Zhu <jhzhu@outlook.com>
2018-09-29 22:19:31 +0800

    Add 6 ml algorithms

--- main.py
+++ main.py
@@ -54,3 +54,29 @@ Y = array[:,4]
 validation_size = 0.20
 seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+
+# Build 6 different algorithms:
+# 1. Logistic Regression (LR)
+# 2. Linear Discriminant Analysis (LDA)
+# 3. K-Nearest Neighbors (KNN).
+# 4. Classification and Regression Trees (CART).
+# 5. Gaussian Naive Bayes (NB).
+# 6. Support Vector Machines (SVM).
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
