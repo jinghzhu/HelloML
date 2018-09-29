86618b8fe7bcca634d8f1c536e8ae7561f588459
Jinghua Zhu <jhzhu@outlook.com>
2018-09-29 21:57:38 +0800

    Add how to get data and check it

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Use pandas to load data set from UCI Machine Learning repository.
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+
+# Check how many instances (rows) and attributes (columns) the data contains with the shape property.
+print(dataset.shape)
+# Get first 20 rows of data.
+print(dataset.head(20))
+# Show the count, mean, the min and max values as well as some percentiles.
+print(dataset.describe())
+# class distribution
+print(dataset.groupby('class').size())
